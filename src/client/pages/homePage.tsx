5dd7bb470f991ff49690dd1ea4a428713bf81cd7
van hung <hungtv290501@gmail.com>
2022-09-09 13:42:51 +0700

    logo title

--- src/client/pages/homePage.tsx
+++ src/client/pages/homePage.tsx
@@ -7,9 +7,6 @@ const HomePage = (props: Props) => {
   return (
     <div>
         <head>
-            <meta charSet='UTF-8' />
-            <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>SunCinema</title>
         </head>
         <SlideShow/>
