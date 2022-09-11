38327a16368f75a8503012aa4e11db2eb0d99a46
taquangnghia <nghiatqph13235@fpt.edu.vn>
2022-09-11 09:49:48 +0700

    UI-Signin,Signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import { Route,Routes } from 'react-router-dom'
-import Layout from './client/layout/layout'
-import HomePage from './client/pages/homePage'
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import Hometab1 from './client/components/homeTab1'
-import Hometab2 from './client/components/homeTab2'
-import MovieDetail from './client/pages/detail'
+import { useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./client/layout/layout";
+import HomePage from "./client/pages/homePage";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Hometab1 from "./client/components/homeTab1";
+import Hometab2 from "./client/components/homeTab2";
+import MovieDetail from "./client/pages/detail";
+import Signin from "./client/pages/signin";
+import Signup from "./client/pages/signup";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
+    <div className="App">
       <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route element={<HomePage/>}>
-            <Route index element={<Hometab1/>}/>
-            <Route path='tab1' element={<Hometab1/>}/>
-            <Route path='tab2' element={<Hometab2/>}/>
+        <Route path="/" element={<Layout />}>
+          <Route element={<HomePage />}>
+            <Route index element={<Hometab1 />} />
+            <Route path="tab1" element={<Hometab1 />} />
+            <Route path="tab2" element={<Hometab2 />} />
           </Route>
-          <Route path='detail' element={<MovieDetail/>}/>
+          <Route path="detail" element={<MovieDetail />} />
         </Route>
+        <Route path="signin" element={<Signin></Signin>} />
+        <Route path="signup" element={<Signup></Signup>} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
